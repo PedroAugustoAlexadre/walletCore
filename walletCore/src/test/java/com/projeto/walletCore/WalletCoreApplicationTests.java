521aed456e6d03dfc57b732ded7a29e97f069bd9
pedro augusto alexandre <pedroAugustoAlexandre@proton.me>
2026-01-17 13:51:57 -0300

    feat: created project

---
+++ walletCore/src/test/java/com/projeto/walletCore/WalletCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.walletCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
